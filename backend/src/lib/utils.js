e258ed2076ce58b1ef6a7191248614990414328d
surendra <surendra1234@gmail.com>
2026-01-08 22:45:18 +0530

    project completed

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,37 @@
+// import jwt from "jsonwebtoken"
+
+// export const generateToken = (userId, res) =>{
+//     const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+//         expiresIn: "7d"
+//     })
+
+//     res.cookie("jwt", token, {
+//         maxAge: 7*24*60*60*1000, //ms
+//         httpOnly: true,  //prevent xss attacks cross-site scripting attacks
+//         sameSite: "strict",  //CSRF attacks cross-site request forgery attacks
+//         secure: process.env.NODE_ENV !== "development"
+//     })
+
+//     return token;
+// }
+
+
+
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign(
+    { userId },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: false,      // ⭐ ALWAYS FALSE IN LOCALHOST
+    sameSite: "lax",    // ⭐ IMPORTANT — strict mat use karo
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  });
+
+  return token;
+};
---
+++ frontend/postcss.config.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//   plugins: {
+//     "@tailwindcss/postcss": {},
+//   },
+// };
+
+
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+}
